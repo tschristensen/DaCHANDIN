2a4928007f943a861f47550919a14be1ffdd007d
Thomas Christensen <thomas.s.christensen101@gmail.com>
2016-03-21 14:41:54 +0100

    added graphics

--- parameterization/parameterization.m
+++ parameterization/parameterization.m
@@ -5,11 +5,19 @@ ra_data_enc_motor = csvread('ra_data_enc_motor.csv',1,0);
 V = ra_data_enc_motor(:,1);
 I = ra_data_enc_motor(:,2);
 
-Ra = sum(V)/sum(I)
+f = fittype('a*x+b');
 
-ra_data_motor = csvread('ra_data_motor.csv',1,0);
-
-V = ra_data_motor(:,1);
-I = ra_data_motor(:,2);
+fit2 = fitlm(V,I)
+%[fit1,gof,fitinfo] = fit(V,I,f,'StartPoint',[1 1]);
 
 Ra = sum(V)/sum(I)
+
+fig_x = 400*2; fig_y = 225*2;
+fitted = fit2.Fitted
+figure('Position',[100,100,fig_x,fig_y])
+plot(V,fitted,'r-',V,I,'o')
+legend('Data Points', 'Fitted Line', 'Location', 'northwest')
+title('Current as a Function of Voltage')
+xlabel('Voltage [V]')
+ylabel('Current [I]')
+
