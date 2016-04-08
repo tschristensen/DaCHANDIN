8341fa485539b0e7f6638971cde21a349d4ebcf0
Thomas Christensen <thomas.s.christensen101@gmail.com>
2016-04-08 15:11:57 +0200

    pull commit

--- contoller_experiment_data/performance.m
+++ contoller_experiment_data/performance.m
@@ -1,10 +1,7 @@
 clc;clear;close all;
 
-%load('PI_125rads_load_step_ex.mat');
-%load('PI_125rads_load_step_unstable_ex.mat');
-
-% load('PI_single_125rads_extra_inertia_ex.mat');
-load('PI_single_125rads_extra_inertia_ex1.mat');
+%%Velocity Step with inertia
+load('PI_single_125rads_extra_inertia_ex.mat');
 
 time = PI_single_125rads_extra_inertia.X.Data;
 w = PI_single_125rads_extra_inertia.Y.Data;
@@ -87,52 +84,53 @@ legend('Step Response', '5% Settling Time Boundary','location','southeast')
 xlim([0,0.25])
 
 
-% load('PID_dual_125rads_load_ex.mat');
-% time = PID_dual_125rads_load_ex.X.Data;
-% w = PID_dual_125rads_load_ex.Y.Data;
-% 
-% index = 1501;
-% avg = sum(w(index:end))/(length(w)-index)
-% S = stepinfo(w,time, 125)
-% l = zeros([1 length(w)]);
-% l_low = l + 125*0.95;
-% l_high = l + 125*1.05;
-% 
-% fig_x = 400*2; fig_y = 225*2;
-% figure('Position',[100,100,fig_x,fig_y])
-% plot(time,w)
-% %plot(time,w,time,l_low,'-.r',time,l_high,'-.r')
-% grid on
-% title('Step Response of PID Controller - Load 0.4[$A$], 125 [$\frac{Rad}{S}$]','interpreter','LaTex')
-% xlabel('Time [$S$]','interpreter','LaTex')
-% ylabel('Angular Velocity [$\frac{Rad}{S}$]','interpreter','LaTex')
-% legend('Step Response', '5% Settling Time Boundary')
+load('PID_dual_125rads_load_ex.mat');
+time = PID_dual_125rads_load_ex.X.Data;
+w = PID_dual_125rads_load_ex.Y.Data;
 
-% load('PID_single_125rads_ex.mat');
-% time = PID_single_125rads_ex.X.Data;
-% w = PID_single_125rads_ex.Y.Data;
-% 
-% index = 1501;
-% avg = sum(w(index:end))/(length(w)-index)
-% S = stepinfo(w,time, 125)
-% l = zeros([1 length(w)]);
-% l_low = l + 125*0.95;
-% l_high = l + 125*1.05;
-% 
-% fig_x = 400*2; fig_y = 225*2;
-% figure('Position',[100,100,fig_x,fig_y])
-% plot(time,w,time,l_low,'-.r',time,l_high,'-.r')
-% grid on
-% title('Step Response of PID Controller - 125 [$\frac{Rad}{S}$]','interpreter','LaTex')
-% xlabel('Time [$S$]','interpreter','LaTex')
-% ylabel('Angular Velocity [$\frac{Rad}{S}$]','interpreter','LaTex')
-% 
-% index = 1501;
-% avg = sum(w(index:end))/(length(w)-index)
-% S = stepinfo(w,time, 125)
-% l = zeros([1 length(w)]);
-% l_low = l + 125*0.95;
-% l_high = l + 125*1.05;
+index = 1501;
+avg = sum(w(index:end))/(length(w)-index)
+S = stepinfo(w,time, 125)
+l = zeros([1 length(w)]);
+l_low = l + 125*0.95;
+l_high = l + 125*1.05;
+
+fig_x = 400*2; fig_y = 225*2;
+figure('Position',[100,100,fig_x,fig_y])
+plot(time,w)
+%plot(time,w,time,l_low,'-.r',time,l_high,'-.r')
+grid on
+title('Step Response of PID Controller - Load 0.4[$A$], 125 [$\frac{Rad}{S}$]','interpreter','LaTex')
+xlabel('Time [$S$]','interpreter','LaTex')
+ylabel('Angular Velocity [$\frac{Rad}{S}$]','interpreter','LaTex')
+legend('Step Response', '5% Settling Time Boundary')
+
+load('PID_single_125rads_ex.mat');
+time = PID_single_125rads_ex.X.Data;
+w = PID_single_125rads_ex.Y.Data;
+
+index = 1501;
+avg = sum(w(index:end))/(length(w)-index)
+S = stepinfo(w,time, 125)
+l = zeros([1 length(w)]);
+l_low = l + 125*0.95;
+l_high = l + 125*1.05;
+
+fig_x = 400*2; fig_y = 225*2;
+figure('Position',[100,100,fig_x,fig_y])
+%plot(time,w,time,l_low,'-.r',time,l_high,'-.r')
+plot(time,w)
+grid on
+title('Step Response of PID Controller - 125 [$\frac{Rad}{S}$]','interpreter','LaTex')
+xlabel('Time [$S$]','interpreter','LaTex')
+ylabel('Angular Velocity [$\frac{Rad}{S}$]','interpreter','LaTex')
+
+index = 1501;
+avg = sum(w(index:end))/(length(w)-index)
+S = stepinfo(w,time, 125)
+l = zeros([1 length(w)]);
+l_low = l + 125*0.95;
+l_high = l + 125*1.05;
 % 
 % load('PID_single_125rads_inertia_ex.mat');
 % time = PID_single_125rads_inertia_ex.X.Data;
